834b070b6c6c01ef916864b15e5468c2ec53513a
Jo Zhouzheng <jo@Jos-MacBook.local>
2018-10-09 21:51:19 -0400

    git

--- app.rb
+++ app.rb
@@ -24,16 +24,6 @@ enable :sessions
 
 class CustomHandler < AlexaSkillsRuby::Handler
 
-  on_intent("GetZodiacHoroscopeIntent") do
-    slots = request.intent.slots
-    response.set_output_speech_text("Horoscope Text")
-    #response.set_output_speech_ssml("<speak><p>Horoscope Text</p><p>More Horoscope text</p></speak>")
-    response.set_reprompt_speech_text("Reprompt Horoscope Text")
-    #response.set_reprompt_speech_ssml("<speak>Reprompt Horoscope Text</speak>")
-    response.set_simple_card("title", "content")
-    logger.info 'GetZodiacHoroscopeIntent processed'
-  end
-
   on_intent("HERE") do
   		# add a response to Alexa
       response.set_output_speech_text("I've updated your status to Here ")
@@ -75,7 +65,7 @@ class CustomHandler < AlexaSkillsRuby::Handler
     logger.info 'BackIn processed'
     update_status "BACK_IN", duration
   end
-  
+
 end
 
 # ----------------------------------------------------------------------
